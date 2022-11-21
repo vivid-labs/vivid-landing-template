1ee0577cc7815bf5fd37dd1e7589bd5a4c975e79
Jorge Zreik <jfzreik@gmail.com>
2022-11-21 00:04:49 -0500

    Resize footer padding

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,7 +6,7 @@ import { LinkButton } from "../components/LinkButton";
 export const Footer = () => {
   return (
     <footer className="bg-extra-strong">
-      <div className="items-center justify-between px-12 py-4 mx-auto row max-w-7xl">
+      <div className="items-center justify-between px-10 py-4 mx-auto sm:px-6 row max-w-7xl">
         <div className="items-center gap-4 row">
           <Image
             src="/images/logo.png"
